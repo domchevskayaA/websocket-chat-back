0e3d31d50ef8aa21ed19370f2f3f2e5e0dc157e9
Olena Domchevska <olena.domchevska@altran.com>
2020-07-08 13:58:27 +0300

    add cors

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 
 const app = express();
+var cors = require('cors');
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.set({
