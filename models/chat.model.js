ad1cddba083edd69f886c14e3dae317560bdfa29
Olena Domchevska <olena.domchevska@altran.com>
2020-08-04 19:01:58 +0300

    move database request to models static methods

--- models/chat.model.js
+++ models/chat.model.js
@@ -10,19 +10,65 @@ const ChatSchema = new mongoose.Schema({
   },
   users: {
     type: [{ type: mongoose.Types.ObjectId, ref: 'User' }]
+  },
+  count: {
+    type: Number,
+    default: 1,
   }
 });
 
+//custom method to generate authToken
+const getPopulateUsersObject = (userId) => {
+  return {
+    path: 'users',
+    select: 'name avatar',
+    match: { _id: {$ne: userId }},
+  };
+};
+
 ChatSchema.statics.getChatsByUserId = function(userId) {
-    return this.find({ users: userId }).populate({
-      path: 'users',
-      select: 'name avatar',
-      match: { _id: {$ne: userId }},
-    })
+    return this.find({ users: userId }).populate(getPopulateUsersObject(userId))
     .catch(err => {
       console.error(err);
       throw err;
     })
+};
+
+ChatSchema.statics.getChatByReceiverId = async function(receiverId) {
+  const sender = getUserFromRequest(req);
+  const filter = {users: {$all: [receiverId, sender._id]}};
+
+  let chat = await this.findOneAndUpdate(
+    filter, {
+      $set: { 'messages.$[].read': true }
+    }, (err, chat) => {
+    }).populate(getPopulateUsersObject(sender._id));
+
+  if (!chat) {
+    chat = new Chat({
+      users: [receiverId, sender._id],
+      messages: []
+    }).populate(populateUsersObject);
+    await chat.save();
+  };
+
+  return chat;
+};
+
+ChatSchema.statics.postChatMessage = async function(filter, message) {
+
+  let chat = await this.findOne(filter);
+
+  if (!chat) {
+    return res.status(404).send("Chat not found.");
+  } else {
+    chat = await this.findOneAndUpdate(
+      filter,
+      {$push: {messages: message}},
+      {new: true})
+  };
+
+  return chat;
 };
 
 const Chat = mongoose.model('Chat', ChatSchema);
--- routes/chats.route.js
+++ routes/chats.route.js
@@ -5,55 +5,33 @@ const express = require("express");
 
 const router = express.Router();
 
+
+// returns all user chats
 router.get("/", async (req, res) => {
   const token = getTokenFromRequest(req);
   const user = await User.getUserByToken(token);
 
-  let chats = await Chat.getChatsByUserId(user._id);
+  const chats = await Chat.getChatsByUserId(user._id);
 
   res.status(200).send(chats);
 });
 
+// returns chat with specific user or creates new one
 router.get("/receiver/:receiver_id", async (req, res) => {
-  const sender = getUserFromRequest(req);
   const receiver_id = req.params.receiver_id;
-  const filter = {users: {$all: [receiver_id, sender._id]}};
 
-  let chat = await Chat.findOneAndUpdate(
-    filter, {
-      $set: { 'messages.$[].read': true }
-    }, (err, chat) => {
-      // console.log(err, chat);
-    }).populate({
-      path: 'users',
-      select: 'name avatar',
-      match: { _id: {$ne: userId }},
-    });
-
-  if (!chat) {
-    chat = new Chat({
-      users: [receiver_id, sender._id],
-      messages: []
-    }).populate({
-      path: 'users',
-      select: 'name avatar',
-      match: { _id: {$ne: userId }},
-    });
-    await chat.save();
-  }
-
+  const chat = await Chat.getChatByReceiverId(receiver_id);
   res.status(200).send(chat);
 });
 
+// add new chat message
 router.post("/:id/messages", async (req, res) => {
   const filter = {_id: req.params.id};
   const sender = getUserFromRequest(req);
   const { receiver_id, text } = req.body;
-
-  let chat = await Chat.findOne(filter);
   const date = new Date();
 
-  const data = {
+  const message = {
     _id: chat.messages ? chat.messages.length : 0,
     sender,
     receiver_id,
@@ -62,14 +40,7 @@ router.post("/:id/messages", async (req, res) => {
     read: false
   };
 
-  if (!chat) {
-    return res.status(404).send("Chat not found.");
-  } else {
-    chat = await Chat.findOneAndUpdate(
-      filter,
-      {$push: {messages: data}},
-      {new: true})
-  }
+  const chat = Chat.postChatMessage(filter, message);
 
   res.status(200).send(chat);
 });
